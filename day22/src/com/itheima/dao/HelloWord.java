836c61c9268ef7dc40edd72b6a6455d58bf11ec6
LLX <827728849@qq.com>
2019-12-30 15:36:36 +0800

    第二次提交

--- day22/src/com/itheima/dao/HelloWord.java
+++ day22/src/com/itheima/dao/HelloWord.java
@@ -3,5 +3,6 @@ package com.itheima.dao;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
+        System.out.println("老七");
     }
 }
